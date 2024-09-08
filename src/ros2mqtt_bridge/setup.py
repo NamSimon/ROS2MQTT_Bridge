068f29a40e61f3608562d30fa7225cb4fa4bc6b8
NamSimon <cknam0708@sju.ac.kr>
2024-09-08 19:03:49 +0900

    2024.09.08

--- src/ros2mqtt_bridge/setup.py
+++ src/ros2mqtt_bridge/setup.py
@@ -4,7 +4,7 @@ package_name = 'ros2mqtt_bridge'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.0.1',  # 버전을 0.0.1로 변경
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,11 +15,12 @@ setup(
     zip_safe=True,
     maintainer='edge',
     maintainer_email='cknam0708@sju.ac.kr',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='A ROS2 package for bridging ROS2 and MQTT communication',
+    license='Apache License 2.0',  # 적절한 라이선스 추가
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'ros2mqtt_bridge = ros2mqtt_bridge.ros2mqtt_bridge:main',  # 스크립트 실행을 위한 엔트리 포인트 추가
         ],
     },
 )
