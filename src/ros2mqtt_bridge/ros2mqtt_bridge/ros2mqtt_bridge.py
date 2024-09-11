7bc61f3a1568725e720e774d94b08dec1862734a
NamSimon <cknam0708@sju.ac.kr>
2024-09-11 17:58:45 +0900

    2024.09.11

--- src/ros2mqtt_bridge/ros2mqtt_bridge/ros2mqtt_bridge.py
+++ src/ros2mqtt_bridge/ros2mqtt_bridge/ros2mqtt_bridge.py
@@ -1,9 +1,8 @@
 import rclpy
 from rclpy.node import Node
 import importlib
-import time
 import os
-from mqtt.mqtt import MQTTClient  # MQTTClient 클래스를 불러옴
+from mqtt.mqtt import MQTTClient
 
 class ROS2MQTTBridge(Node):
     def __init__(self):
@@ -14,14 +13,14 @@ class ROS2MQTTBridge(Node):
         self.ros2mqtt_ros_topic = os.getenv('ROS2MQTT_ROS_TOPIC', '')
         self.ros2mqtt_ros_type = os.getenv('ROS2MQTT_ROS_TYPE', '')  # ROS 메시지 타입
         self.mode = os.getenv('MODE')  # 모드 ('pub' 또는 'sub')
-        print(self.mode)
+
         # ROS 메시지 타입을 동적으로 로드
         self.ros_msg_type = self.get_ros_msg_type(self.ros2mqtt_ros_type)
 
         # MQTT 클라이언트를 설정하고 메시지 콜백을 등록
         self.mqtt_client = MQTTClient(self.on_mqtt_message_received)
+
         if self.platform == 'edge':
-        # ROS 퍼블리셔 및 구독자 설정
             if self.mode == 'pub':
                 self.mqtt_client.subscribe()  # MQTT 구독 시작
                 # ROS -> MQTT
@@ -45,10 +44,7 @@ class ROS2MQTTBridge(Node):
                 self.get_logger().error("올바르지 않은 모드 설정. 'pub' 또는 'sub'만 허용됩니다.")
                 return
             
-        elif self.platform =='user':
-            # ROS 퍼블리셔 및 구독자 설정
-
-
+        elif self.platform == 'user':
             if self.mode == 'pub': 
                 # MQTT -> ROS
                 self.get_logger().info("모드: pub - MQTT 구독, ROS 퍼블리시")
@@ -69,25 +65,18 @@ class ROS2MQTTBridge(Node):
                     10
                 )
 
-                
             else:
                 self.get_logger().error("올바르지 않은 모드 설정. 'pub' 또는 'sub'만 허용됩니다.")
                 return
-            
 
     def get_ros_msg_type(self, ros_type_name):
         """ROS 메시지 타입을 동적으로 로드하는 함수."""
-        # 메시지 타입 형식이 '패키지/메시지' 형식을 따르는지 확인
-        
-        # "msg/"와 같은 불필요한 경로가 포함된 경우 제거
         if '/msg/' in ros_type_name:
             ros_type_name = ros_type_name.replace('/msg/', '/')
-        print(ros_type_name)
-        # 패키지명과 메시지명 분리
+        
         package_name, msg_name = ros_type_name.split('/')
         
         try:
-            # 패키지에서 메시지 모듈 가져오기
             module = importlib.import_module(f"{package_name}.msg")
             msg_type = getattr(module, msg_name, None)
 
@@ -95,7 +84,6 @@ class ROS2MQTTBridge(Node):
                 self.get_logger().error(f"Message type {ros_type_name} not found in package {package_name}")
                 return None
             
-            # 타입 서포트를 확인하여 ROS 2 메시지 타입인지 체크
             if not hasattr(msg_type, '_TYPE_SUPPORT'):
                 self.get_logger().error(f"Message type {ros_type_name} is missing _TYPE_SUPPORT. Ensure it's a valid ROS 2 message.")
                 return None
@@ -108,13 +96,23 @@ class ROS2MQTTBridge(Node):
     def ros_to_mqtt_callback(self, msg):
         """ROS에서 수신된 데이터를 MQTT로 퍼블리시하는 콜백 함수."""
         self.get_logger().info(f"ROS에서 데이터 수신: {msg}")
-        self.mqtt_client.publish(msg.data)
+
+        # ROS 메시지를 딕셔너리로 변환하여 MQTT로 전송
+        data = {}
+        for field_name in msg.__slots__:
+            data[field_name] = getattr(msg, field_name)
+        self.mqtt_client.publish(data)
 
     def on_mqtt_message_received(self, data):
         """MQTT에서 수신된 메시지를 ROS로 퍼블리시."""
         self.get_logger().info("MQTT 메시지를 ROS로 퍼블리시합니다.")
-        ros_msg = self.ros_msg_type()
-        ros_msg.data = data['data']
+        ros_msg = self.ros_msg_type()  # 동적으로 로드된 메시지 타입 인스턴스 생성
+
+        # 수신된 MQTT 데이터를 ROS 메시지 필드에 동적으로 설정
+        for field_name, value in data.items():
+            if hasattr(ros_msg, field_name):
+                setattr(ros_msg, field_name, value)
+        
         self.ros_publisher.publish(ros_msg)
 
 def main(args=None):
